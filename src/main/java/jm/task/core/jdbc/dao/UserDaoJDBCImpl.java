7e5ba579f982bb0713bd534c28010c38af54339e
Nikita Pastukhov <pastukhovns@yandex.ru>
2024-02-28 23:16:52 +0500

    Completed the task

--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -1,35 +1,121 @@
 package jm.task.core.jdbc.dao;
 
 import jm.task.core.jdbc.model.User;
-
+import jm.task.core.jdbc.util.Util;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class UserDaoJDBCImpl implements UserDao {
-    public UserDaoJDBCImpl() {
 
+    Connection connection = Util.getConnection();
+
+    public UserDaoJDBCImpl() {
     }
 
     public void createUsersTable() {
-
+        PreparedStatement preparedStatement = null;
+        String sql = "CREATE TABLE IF NOT EXISTS usertable (" +
+                "id BIGINT PRIMARY KEY NOT NULL AUTO_INCREMENT," +
+                " name VARCHAR(100) NOT NULL," +
+                " lastName VARCHAR(100) NOT NULL," +
+                " age TINYINT NOT NULL)";
+        try {
+            preparedStatement = connection.prepareStatement(sql);
+            preparedStatement.execute();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     public void dropUsersTable() {
-
+        PreparedStatement preparedStatement = null;
+        String sql = "DROP TABLE IF EXISTS usertable";
+        try {
+            preparedStatement = connection.prepareStatement(sql);
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     public void saveUser(String name, String lastName, byte age) {
-
+        PreparedStatement preparedStatement = null;
+        String sql = "INSERT INTO usertable (name, lastName, age) " +
+                "VALUES (?, ?, ?)";
+        try {
+            preparedStatement = connection.prepareStatement(sql);
+            preparedStatement.setString(1, name);
+            preparedStatement.setString(2, lastName);
+            preparedStatement.setByte(3, age);
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (preparedStatement != null) {
+                    preparedStatement.close();
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
     public void removeUserById(long id) {
-
+        PreparedStatement preparedStatement = null;
+        String sql = "DELETE FROM usertable WHERE id = ?";
+        try {
+            preparedStatement = connection.prepareStatement(sql);
+            preparedStatement.setLong(1, id);
+            preparedStatement.execute();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     public List<User> getAllUsers() {
-        return null;
+        List<User> userList = new ArrayList<>();
+        PreparedStatement preparedStatement = null;
+        String sql = "SELECT * FROM usertable";
+        try {
+            preparedStatement = connection.prepareStatement(sql);
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+            while(resultSet.next()) {
+                User user = new User();
+                user.setId(resultSet.getLong("id"));
+                user.setName(resultSet.getString("name"));
+                user.setLastName(resultSet.getString("lastName"));
+                user.setAge(resultSet.getByte("age"));
+
+                userList.add(user);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (preparedStatement != null) {
+                    preparedStatement.close();
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+        return userList;
     }
 
     public void cleanUsersTable() {
-
+        PreparedStatement preparedStatement = null;
+        String sql = "DELETE FROM usertable";
+        try {
+            preparedStatement = connection.prepareStatement(sql);
+            preparedStatement.execute();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,29 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Util {
     // реализуйте настройку соеденения с БД
+    private static final String DB_URL = "jdbc:mysql://localhost:3306/firstbd?useUnicode=true&useSSL=true&useJDBCCompliantTimezoneShift=true" +
+            "&useLegacyDatetimeCode=false&serverTimezone=UTC";
+    private static final String DB_USERNAME = "root";
+    private static final String DB_PASSWORD = "root";
+
+    public static Connection getConnection() {
+        Connection connection = null;
+        try {
+            connection = DriverManager.getConnection(DB_URL, DB_USERNAME, DB_PASSWORD);
+//            if (!connection.isClosed()) {
+//                System.out.println("Соединение с БД Установлено!");
+//            }
+//            if (connection.isClosed()) {
+//                System.out.println("Соединение с БД Закрыто!");
+//            }
+        } catch (SQLException e) {
+            System.out.println("Connection error");
+        }
+        return connection;
+    }
 }
