947fcfe2f18297abd333381494e7166d2bdf1435
Nikita Pastukhov <pastukhovns@yandex.ru>
2024-04-04 21:32:35 +0500

    hiberbate 1

--- src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
@@ -1,10 +1,28 @@
 package jm.task.core.jdbc.dao;
 
 import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.service.UserServiceImpl;
+import jm.task.core.jdbc.util.Util;
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 
+import javax.management.Query;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
+
 public class UserDaoHibernateImpl implements UserDao {
+
+    private Transaction transaction = null;
+
     public UserDaoHibernateImpl() {
 
     }
@@ -12,31 +30,147 @@ public class UserDaoHibernateImpl implements UserDao {
 
     @Override
     public void createUsersTable() {
+        try (Session session = Util.getSessionFactory().openSession()) {
+            transaction = session.beginTransaction();
+
+            String sql = "CREATE TABLE IF NOT EXISTS usertable " +
+                    "(id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY, " +
+                    "name VARCHAR(50) NOT NULL, lastName VARCHAR(50) NOT NULL, " +
+                    "age TINYINT NOT NULL)";
+
+            Query query = (Query) session.createSQLQuery(sql).addEntity(User.class);
+
+            transaction.commit();
+//            session.close();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            e.printStackTrace();
+        }
 
     }
 
     @Override
     public void dropUsersTable() {
+        try (Session session = Util.getSessionFactory().openSession()) {
+            transaction = session.beginTransaction();
 
+            String sql = "DROP TABLE IF EXISTS usertable";
+
+            Query query = (Query) session.createSQLQuery(sql).addEntity(User.class);
+
+            transaction.commit();
+            session.close();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            e.printStackTrace();
+        }
     }
 
     @Override
     public void saveUser(String name, String lastName, byte age) {
+        try (Session session = Util.getSessionFactory().openSession()) {
+            User user = new User();
+            user.setName(name);
+            user.setLastName(lastName);
+            user.setAge(age);
 
+            session.save(user);
+            transaction.commit();
+//            session.close();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            e.printStackTrace();
+        }
     }
 
     @Override
     public void removeUserById(long id) {
-
+        try (Session session = Util.getSessionFactory().openSession()) {
+            transaction = session.beginTransaction();
+            User user = (User) session.find(User.class, id);
+            session.remove(user);
+            session.flush();
+//            session.close();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            e.printStackTrace();
+        }
     }
 
     @Override
     public List<User> getAllUsers() {
-        return null;
-    }
+        List<User> userList = new ArrayList<>();
+
+        try (Session session = Util.getSessionFactory().openSession()
+             ) {
+            transaction = session.beginTransaction();
+            CriteriaBuilder builder = session.getCriteriaBuilder();
+
+            // Create CriteriaQuery
+            CriteriaQuery<User> criteria = builder.createQuery(User.class);
+
+            // Specify criteria root
+            criteria.from(User.class);
+
+            // Execute query
+            List<User> resultList = session.createQuery(criteria).getResultList();
+            for (User e : resultList) {
+                // load the data
+                userList.add(e);
+            }
+
+//            Query query = (Query) session.createSQLQuery(sql).addEntity(User.class);
+//            ResultSet resultSet = session.executeUpdate();
+//            while (resultSet.next()) {
+//                User user = new User();
+//                user.setId(resultSet.getLong("id"));
+//                user.setName(resultSet.getString("name"));
+//                user.setLastName(resultSet.getString("lastName"));
+//                user.setAge(resultSet.getByte("age"));
+//
+//                userList.add(user);
+
+//            CriteriaBuilder builder = session.getCriteriaBuilder();
+//            CriteriaQuery<T> criteria = builder.createQuery(type);
+//            criteria.from(type);
+//            List<T> data = session.createQuery(criteria).getResultList();
+//            }
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            e.printStackTrace();
+        }
+//
+//
+        return userList;
+//            return UserServiceImpl.getSession().createQuery("from Service").list();
+
+
+        }
 
     @Override
     public void cleanUsersTable() {
+        try (Session session = Util.getSessionFactory().openSession()) {
+            transaction = session.beginTransaction();
+            String sql = "delete from usertable";
 
+            session.remove(sql);
+            session.flush();
+//            session.close();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            e.printStackTrace();
+        }
     }
 }
--- src/main/java/jm/task/core/jdbc/model/User.java
+++ src/main/java/jm/task/core/jdbc/model/User.java
@@ -1,21 +1,31 @@
 package jm.task.core.jdbc.model;
 
+
 import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
-@Table
+
+@Entity
+@Table(name = "usertable")
 public class User {
+
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
     private Long id;
 
-    @Column
+    @Column(name = "name")
     private String name;
 
-    @Column
+    @Column(name = "lastName")
     private String lastName;
 
-    @Column
+    //можно не писать @Column(name = "age"), так как имя совпадает с переменной
+    @Column(name = "age")
     private Byte age;
 
     public User() {
